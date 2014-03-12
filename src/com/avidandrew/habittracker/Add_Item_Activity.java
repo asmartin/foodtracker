2e4fd2992a7079f8ae63eb6aaf126c436db556bd
Emmanuel Contreras <metalshreds@gmail.com>
2014-03-11 20:39:44 -0500

    add item activity now launches

--- src/com/avidandrew/habittracker/Add_Item_Activity.java
+++ src/com/avidandrew/habittracker/Add_Item_Activity.java
@@ -2,6 +2,7 @@ package com.avidandrew.habittracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView.FindListener;
@@ -10,27 +11,38 @@ import android.widget.Button;
 import com.example.first_app.R;
 
 public class Add_Item_Activity extends Activity{
+	
+	public Add_Item_Activity(){
+		//TODO
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.add_item_activity);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
 		
 		final Button button = (Button) findViewById(R.id.add_item);
 		button.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
+				// TODO
 				
 			}
 		});
+		
+	
 	}
 	
-	
-	
-	
-	
-	
-	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }
