9e9b434394fe1f7e11e87ad8f6f587c5e39f1271
Emmanuel Contreras <metalshreds@gmail.com>
2014-03-17 22:05:09 -0500

    added About Activity and button styles

---
+++ src/com/avidandrew/habittracker/About_Activity.java
@@ -0,0 +1,32 @@
+package com.avidandrew.habittracker;
+
+import com.example.first_app.R;
+import com.example.first_app.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class About_Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.about_activity);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+	
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
+
+
+}
---
+++ src/com/avidandrew/habittracker/Timestamp.java
@@ -0,0 +1,19 @@
+package com.avidandrew.habittracker;
+
+public class Timestamp {
+	
+	int itemID;
+	String name;
+	String timestamp;
+
+	public Timestamp(int itemID, String name, String timestamp) {
+		
+		this.itemID = itemID;
+		this.name = name;
+		this.timestamp = timestamp;
+		
+		
+		
+	}
+
+}
